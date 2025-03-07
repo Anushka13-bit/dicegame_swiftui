53cc8d6a0b41b3ddd5fe4c40a578fb4d9d1e4190
Anushka13-bit <anushkagattani1@gmail.com>
2025-03-07 10:23:30 +0530

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  dicegame
+//
+//  Created by Anushka Gattani on 07/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var dice = "1"
+
+    var body: some View {
+        ZStack {
+
+            
+
+            VStack {
+                // Dice image
+                Image(dice)
+                    .resizable()
+                    .frame(width: 300, height: 300)
+
+                // Roll dice button
+                Button(action: {
+                    self.rollDice()
+                }) {
+                    HStack {
+                        Image(systemName: "button.horizontal.fill")
+                        Text("ROLL DICE")
+                    }
+                }
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.gray)
+                .cornerRadius(20)
+            }
+        }
+    }
+
+
+    func rollDice() {
+        let diceno = Int.random(in: 1...6)
+        dice = "\(diceno)"
+    }
+}
+
+#Preview {
+    ContentView()
+}
